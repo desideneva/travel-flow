1d223b14284b4f13e37150b21640d18a77f34265
Desislava Deneva <tolteki3@abv.bg>
2024-01-23 02:32:40 +0000

    Import sys module for accessing system-specific parameters and functions

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import sys
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -14,7 +15,6 @@ SHEET = GSPREAD_CLIENT.open('travel_flow')
 destinations_worksheet = SHEET.worksheet("destinations")
 
 destinations_countries_list = destinations_worksheet.col_values(2)[2:]
-
 
 def get_user_preferences():
     """
@@ -30,19 +30,22 @@ def get_user_preferences():
         user_choice = input("Enter the number corresponding to your preference: ")
 
         if user_choice.isdigit() and 1 <= int(user_choice) <= 6:
-            return int(user_choice)
+            if int(user_choice) == 1:
+                get_user_adventure_preference()
+            elif int(user_choice) == 2:
+                get_user_cultural_preference()
+            elif int(user_choice) == 3:
+                get_user_active_preference()
+            elif int(user_choice) == 4:
+                get_user_relaxing_preference()
+            elif int(user_choice) == 5:
+                get_user_family_preference()
+            elif int(user_choice) == 6:
+                get_user_solo_preference()
         else:
             print("Invalid input. Please enter a number between 1 and 6.")
 
-user_preference = get_user_preferences()
-print(f"You chose preference number {user_preference}.")
-
-# Function to get user preference for Adventure or Explorer
 def get_user_adventure_preference():
-    """
-    Display options for Adventure and Explorer and get user's choice.
-    Returns the user's choice as a string.
-    """
     print("Please choose your preference:")
     print("1. Adventure")
     print("2. Explorer")
@@ -50,28 +53,115 @@ def get_user_adventure_preference():
     while True:
         user_choice = input("Enter the number of your preference: ")
         if user_choice in ["1", "2"]:
-            return user_choice
+            display_countries_for_adventure(user_choice)
         else:
             print("Invalid choice. Please enter 1 or 2.")
 
-# Function to display countries based on user's preference
-def display_countries_for_adventure(get_user_adventure_preference):
-    """
-    Display the list of countries based on user's preference.
-    Returns the list of countries.
-    """
-    if user_preference == "1":
-        # Display countries for Adventure
-        adventure_countries = ["New Zealand", "Nepal", "Canada", "Australia", "Costa Rica", "Norway", "Sri Lanka", "Chile", "Bali", "South Africa"]
-        print("Here are the countries for Adventure:")
-        print(", ".join(adventure_countries))
-        return adventure_countries
-    elif user_preference == "2":
-        # Display countries for Explorer
-        explorer_countries = ["Mongolia", "Papua New Guinea", "Bhutan", "Ethiopia", "Madagascar", "Suriname", "Guyana", "Namibia", "Kyrgyzstan", "Laos"]
-        print("Here are the countries for Explorer:")
-        print(", ".join(explorer_countries))
-        return explorer_countries
+    def display_countries_for_adventure(user_preference):
+        """
+        Display the list of countries based on user's preference.
+        Returns the list of countries.
+        """
+        if user_preference == "1":
+            # Display countries for Adventure
+            adventure_countries = countries_list = destinations_worksheet.col_values(int(user_preference) + 2)[2:]
+            print("Here are the countries for Adventure:")
+            print(", ".join(adventure_countries))
+            handle_country_selection(countries_list)
+        elif user_preference == "2":
+            # Display countries for Explorer
+            destinations_countries_list = destinations_worksheet.col_values(6)[2:]
+            print("Here are the countries for Explorer:")
+            print(", ".join(explorer_countries))
+            handle_country_selection(countries_list)
+        
+
+def get_user_cultural_preference():
+    print("Please choose your cultural preference:")
+    print("1. In-depth Cultural")
+    print("2. Food & Culinary")
+
+    while True:
+        user_preference = input("Enter the number of your preference: ")
+        if user_preference in ["1", "2"]:
+            display_countries_for_cultural(user_preference)
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+        
+def display_countries_for_cultural(cultural_preference):
+    countries_list = destinations_worksheet.col_values(int(cultural_preference) + 1)[1:]
+    print(f"Here are the countries for In-depth Cultural/Food & Culinary:")
+    print(", ".join(countries_list))
+    handle_country_selection(countries_list)
+
+
+def get_user_active_preference():
+    print("Please choose your active preference:")
+    print("1. Hiking and Trekking")
+    print("2. Skiing")
+
+    while True:
+        user_choice = input("Enter the number of your preference: ")
+        if user_choice in ["1", "2"]:
+            display_countries_for_active(user_choice)
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
+def display_countries_for_active(active_preference):
+    countries_list = destinations_worksheet.col_values(int(active_preference) + 1)[1:]
+    print(f"Here are the countries for Hiking and Trekking/Skiing:")
+    print(", ".join(countries_list))
+    handle_country_selection(countries_list)
+
+# Only one option for Relaxing
+def get_user_relaxing_preference():
+    print("Please choose your relaxing preference:")
+    print("1. Health, Spa & Wellbeing")
+
+    while True:
+        user_choice = input("Enter the number of your preference: ")
+        if user_choice == "1":
+            display_countries_for_relaxing(user_choice)
+            break
+        else:
+            print("Invalid choice. Please enter 1.")
+
+def display_countries_for_relaxing(relaxing_preference):
+    countries_list = destinations_worksheet.col_values(int(relaxing_preference) + 1)[1:]
+    print(f"Here are the countries for Health, Spa & Wellbeing:")
+    print(", ".join(countries_list))
+    handle_country_selection(countries_list)
+
+# Only one option for Family
+def get_user_family_preference():
+    print("Please choose your family preference:")
+    print("1. Family")
+
+    while True:
+        user_choice = input("Enter the number of your preference: ")
+        if user_choice == "1":
+            display_countries_for_family(user_choice)
+            break
+        else:
+            print("Invalid choice. Please enter 1.")
+
+def display_countries_for_family(family_preference):
+    countries_list = destinations_worksheet.col_values(int(family_preference) + 1)[1:]
+    print(f"Here are the countries for Family:")
+    print(", ".join(countries_list))
+    handle_country_selection(countries_list)
+
+def handle_country_selection(selected_countries):
+    while True: 
+            # Get user's input for the country
+            chosen_country = input("Enter the country you want to explore: ")
+            # Validate the entered country
+            if is_valid_country(chosen_country, selected_countries):
+                print(f"You chose {chosen_country}. Let's plan your adventure!")
+                restart()
+            else:
+                print("This country is not on our destinations list. Please, choose one from the list.")
+
 
 # Function to check if the entered country is in the list
 def is_valid_country(country, countries_list):
@@ -81,131 +171,33 @@ def is_valid_country(country, countries_list):
     """
     return country in countries_list
 
-# Get user's preference
-user_preference = get_user_adventure_preference()
-
-# Display countries based on user's preference
-selected_countries = display_countries_for_adventure(get_user_adventure_preference)
-
-# Loop for choosing countries until a valid one is entered
-while True:
-    # Get user's input for the country
-    chosen_country = input("Enter the country you want to explore: ")
-
-    # Validate the entered country
-    if is_valid_country(chosen_country, selected_countries):
-        print(f"You chose {chosen_country}. Let's plan your adventure!")
-        break
-    else:
-        print("This country is not on our destinations list. Please, choose one from the list.")
-
-# Function to get user preference for Cultural
-def get_user_cultural_preference():
-    """
-    Display options for Cultural and get user's choice.
-    Returns the user's choice as a string.
-    """
-    print("Please choose your cultural preference:")
-    print("1. In-depth Cultural")
-    print("2. Food & Culinary")
-
+def restart():
     while True:
-        user_choice = input("Enter the number of your preference: ")
-        if user_choice in ["1", "2"]:
-            return user_choice
+        choice = input("Do you want to start again? yes / no\n ")
+
+        if choice in ["yes", "no"]:
+            if choice == "yes":
+                get_user_preferences()
+            elif choice == "no":
+                sys.exit()
         else:
-            print("Invalid choice. Please enter 1 or 2.")
-        
-def display_countries_for_cultural(cultural_preference):
-    """
-    Display the list of countries based on user's cultural preference.
-    Returns the list of countries.
-    """
-    if cultural_preference == "1":
-        # Display countries for In-depth Cultural
-        in_depth_cultural_countries = ["Japan", "Peru", "Morocco", "India", "Thailand", "Mexico", "Greece", "Italy", "France", "Egypt"]
-        print("Here are the countries for In-depth Cultural:")
-        print(", ".join(in_depth_cultural_countries))
-        return in_depth_cultural_countries
-    elif cultural_preference == "2":
-        # Display countries for Food & Culinary
-        food_culinary_countries = ["Italy", "Japan", "Mexico", "Thailand", "Peru", "Morocco", "Costa Rica", "Canada", "Netherlands", "Vietnam"]
-        print("Here are the countries for Food & Culinary:")
-        print(", ".join(food_culinary_countries))
-        return food_culinary_countries
+            print("Invalid option. Type yes or no")
 
-# Get user's cultural preference
-user_cultural_preference = get_user_cultural_preference()
+get_user_preferences()
 
-# Display countries based on user's cultural preference
-selected_cultural_countries = display_countries_for_cultural(user_cultural_preference)
+         
 
-# Loop for choosing countries until a valid one is entered
-while True:
-    # Get user's input for the country
-    chosen_cultural_country = input("Enter the country you want to explore: ")
+ 
 
-    # Validate the entered country
-    if is_valid_country(chosen_cultural_country, selected_cultural_countries):
-        print(f"You chose {chosen_cultural_country}. Let's explore the cultural richness!")
-        break
-    else:
-        print("This country is not on our destinations list. Please, choose one from the list.")
 
-# Function to get user preference for Active
-def get_user_active_preference():
-    """
-    Display options for Active and get user's choice.
-    Returns the user's choice as a string.
-    """
-    print("Please choose your active preference:")
-    print("1. Hiking and Trekking")
-    print("2. Skiing")
-
-    while True:
-        user_choice = input("Enter the number of your preference: ")
-        if user_choice in ["1", "2"]:
-            return user_choice
-        else:
-            print("Invalid choice. Please enter 1 or 2.")
-
-# Function to display countries based on user's active preference
-def display_countries_for_active(active_preference):
-    """
-    Display the list of countries based on user's active preference.
-    Returns the list of countries.
-    """
-    if active_preference == "1":
-        # Display countries for Hiking and Trekking
-        hiking_countries = ["Nepal", "Peru", "Morocco", "India", "Italy", "Thailand", "Switzerland", "New Zealand", "Austria", "France"]
-        print("Here are the countries for Hiking and Trekking:")
-        print(", ".join(hiking_countries))
-        return hiking_countries
-    elif active_preference == "2":
-        # Display countries for Skiing
-        skiing_countries = ["Switzerland", "Austria", "Canada", "Italy", "France", "Finland", "Norway", "Sweden", "Japan", "United States"]
-        print("Here are the countries for Skiing:")
-        print(", ".join(skiing_countries))
-        return skiing_countries
-
-# Get user's active preference
-user_active_preference = get_user_active_preference()
-
-# Display countries based on user's active preference
-selected_active_countries = display_countries_for_active(user_active_preference)
-
-# Loop for choosing countries until a valid one is entered
-while True:
-    # Get user's input for the country
-    chosen_active_country = input("Enter the country you want to explore: ")
-
-    # Validate the entered country
-    if is_valid_country(chosen_active_country, selected_active_countries):
-        print(f"You chose {chosen_active_country}. Let's plan your active adventure!")
-        break
-    else:
-        print("This country is not on our destinations list. Please, choose one from the list.")
+ 
 
 
 
+
+
+
+
+
+ 
 
